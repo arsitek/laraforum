b9c4ed7fe52912411e757543d4a4312605ad7e4c
arsitek <chandra.ghv@gmail.com>
2019-12-24 20:02:47 +0700

    relationships antar database/model

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -2,9 +2,26 @@
 
 namespace App\Model;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    //pertanyaan selalu berhubungan dengan user (sipenanya)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    //question may has many replies
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
+    //pertanyaan juga berada dalam ketegori
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -2,9 +2,26 @@
 
 namespace App\Model;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    //balasan adalah milik dari pertanyaan tertentu
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    //setiap balasan adalah juga milik user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    //setiap balasan juga memilik banyak like
+    public function like()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
