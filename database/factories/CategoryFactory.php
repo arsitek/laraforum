d8898ce9253a8bcb0e68b0115456f5d4568be13f
arsitek <chandra.ghv@gmail.com>
2019-12-24 21:25:53 +0700

    Factory and seed

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -5,8 +5,14 @@
 use App\Model\Category;
 use Faker\Generator as Faker;
 
+use Illuminate\Support\Str;
+
 $factory->define(Category::class, function (Faker $faker) {
+
+    $word = $faker->word;
+
     return [
-        //
+        'name' => $word,
+        'slug' => Str::slug($word)
     ];
 });
