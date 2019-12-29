8c48cace0a586a5fba2fb57c498df68029bbdf65
arsitek <chandra.ghv@gmail.com>
2019-12-29 08:05:32 +0700

    Vuetify, Vue Router and some components

---
+++ resources/js/components/AppBeranda.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <toolbar></toolbar>
+        <h1>Tes</h1>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import toolbar from "./Toolbar"
+import AppFooter from "./AppFooter"
+export default {
+    components: { toolbar, AppFooter }
+};
+</script>
+
+<style></style>
